3411012742ede95f1d28dbe1bea3ffc4d6ca2ffd
kaysansalman-cyber <kaysan.salman.ui.ac.id>
2026-09-14 13:19:11 +0700

    feat: add project admin, view, and routing

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import show_main, show_experience
-
+from .views import show_main, show_experience, show_projects
 
 
 urlpatterns = [
     path("", show_main, name="show_main"),
     path("experience/", show_experience, name="show_experience"),
+    path("projects/", show_projects, name="show_projects"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from .models import Experience
-
+from .models import Experience, Project
 
 def show_main(request):
     context = {
@@ -21,3 +20,12 @@ def show_experience(request):
     }
 
     return render(request, 'experience.html', context)
+
+def show_projects(request):
+    projects = Project.objects.all()
+
+    context = {
+        'projects': projects
+    }
+
+    return render(request, 'projects.html', context)
